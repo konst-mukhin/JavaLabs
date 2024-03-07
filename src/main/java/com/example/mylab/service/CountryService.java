50b1054250ae1d204e68f2c36c2921b4196161cd
op1umk1d <konstantin.mukhinn@gmail.com>
2024-03-07 14:31:14 +0300

    Fixed naming error

--- src/main/java/com/example/mylab/service/CountryService.java
+++ src/main/java/com/example/mylab/service/CountryService.java
@@ -9,13 +9,13 @@ import java.util.HashMap;
 
 @Service
 public class CountryService {
-    private static final String url = "https://restcountries.com/v3.1/name/{countryName}?fields=capital";
+    private static final String URL = "https://restcountries.com/v3.1/name/{countryName}?fields=capital";
     public CountryModel getCountryInfo(String countryName) {
         HashMap<String, String> parameters = new HashMap<String, String>();
         parameters.put("countryName", countryName);
 
         CapitalContainer[] capitalContainers = new RestTemplate().getForEntity(
-                                                   url,
+                                                   URL,
                                                    CapitalContainer[].class,
                                                    parameters).getBody();
 
