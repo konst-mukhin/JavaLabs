6d53c69b7de6524fc61ebb1a73dca0b4c0f6252e
op1umk1d <konstantin.mukhinn@gmail.com>
2024-03-07 14:12:32 +0300

    Revert "Fixed errors"
    This reverts commit 71d1883c3b84e3cb95f945872180e11334ea7efb.

--- src/test/java/com/example/mylab/MylabApplicationTests.java
+++ src/test/java/com/example/mylab/MylabApplicationTests.java
@@ -1,22 +1,13 @@
 package com.example.mylab;
 
-import org.apache.catalina.core.ApplicationContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 @SpringBootTest
 class MylabApplicationTests {
-	private final ApplicationContext context;
-
-	MylabApplicationTests(ApplicationContext context) {
-		this.context = context;
-	}
 
 	@Test
 	void contextLoads() {
-		assertNotNull(context);
 	}
 
 }
