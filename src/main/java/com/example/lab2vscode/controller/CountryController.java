0e93720151b927a3d07b8e9b01885478fbb370fd
Konstantin Mukhin <konstantin.mukhinn@gmail.com>
2024-03-29 14:45:13 +0300

    remove

--- src/main/java/com/example/lab2vscode/controller/CountryController.java
+++ src/main/java/com/example/lab2vscode/controller/CountryController.java
@@ -9,7 +9,6 @@ import com.example.lab2vscode.service.CountryService;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/example/lab2vscode/controller/LanguageController.java
+++ src/main/java/com/example/lab2vscode/controller/LanguageController.java
@@ -3,7 +3,6 @@ package com.example.lab2vscode.controller;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
