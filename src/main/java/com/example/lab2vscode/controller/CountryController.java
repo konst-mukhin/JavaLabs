a41185d728565fb871b737c7c88583fadcfd2971
Konstantin Mukhin <konstantin.mukhinn@gmail.com>
2024-04-01 21:15:23 +0300

    delete

--- src/main/java/com/example/lab2vscode/controller/CountryController.java
+++ src/main/java/com/example/lab2vscode/controller/CountryController.java
@@ -6,10 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.lab2vscode.model.Country;
 import com.example.lab2vscode.service.CountryService;
 
+import lombok.AllArgsConstructor;
+
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,8 +22,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @RequestMapping("/country")
 @RestController
+@AllArgsConstructor
 public class CountryController {
-    @Autowired
     private CountryService countryService;
 
     @PostMapping("/create")
--- src/main/java/com/example/lab2vscode/controller/RegionController.java
+++ src/main/java/com/example/lab2vscode/controller/RegionController.java
@@ -3,7 +3,6 @@ package com.example.lab2vscode.controller;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,10 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.lab2vscode.model.Region;
 import com.example.lab2vscode.service.RegionService;
 
+import lombok.AllArgsConstructor;
+
 @RequestMapping("/region")
 @RestController
+@AllArgsConstructor
 public class RegionController {
-    @Autowired
     private RegionService regionService;
 
     @PostMapping("/create")
